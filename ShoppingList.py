f10b00537a51ae9515deac41d4d6565fc30acb0c
ztiwoler0neb <ztiwoler0neb@users.noreply.github.com>
2015-10-18 17:43:19 +0100

    Create ShoppingList.py

---
+++ ShoppingList.py
@@ -0,0 +1,24 @@
+__author__ = 'Saturn'
+shopping_list = ["banana", "orange", "apple"]
+
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+
+def compute_bill(food):
+    total = 0
+    for item in food:
+        if stock[item] > 0:
+            total += prices[item]
+            stock[item] -= 1
+    return total
